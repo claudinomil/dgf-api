8fe01b1fcfd2a3c4460e7fa0b827ed4da04f92a7
claudinomil <claudinomoraes@yahoo.com.br>
2024-03-11 11:13:20 -0300

    Acerto Dashboards 1

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,35 @@ Route::prefix('modulos/submodulos')->group(function () {
     Route::get('/{ids}', [SubmoduloController::class, 'admin_modulos_submodulos_in']);
 });
 
+//Limpar Caches via Navegador - Início''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Route::get('/clear-all-cache', function() {
+    $retorno = '';
+
+    //Limpar cache do aplicativo:
+    Artisan::call('cache:clear');
+    $retorno .= 'cache:clear'.'<br>';
+
+    //Limpar cache de rota:
+    Artisan::call('route:cache');
+    $retorno .= 'route:cache'.'<br>';
+
+    //Limpar cache de configuração:
+    Artisan::call('config:cache');
+    $retorno .= 'config:cache'.'<br>';
+
+    //Clear view cache:
+    Artisan::call('view:clear');
+    $retorno .= 'view:clear'.'<br>';
+
+    //Limpe todo o aplicativo de todos os tipos de cache:
+    Artisan::call('optimize:clear');
+    $retorno .= 'optimize:clear'.'<br>';
+
+    echo $retorno;
+});
+//Limpar Caches via Navegador - Fim'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
 
 
 
